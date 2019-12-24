0b24e17c4e8812dcc426365222c37791828bd8aa
Simon <simonhylleberg@gmail.com>
2019-12-25 00:48:21 +0100

    Add project files.

---
+++ BasicExercises/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace BasicExercises
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Task currentTask = new Task();
+            //2
+            Console.WriteLine(currentTask.task2(2, 7));
+            //1
+           // Console.WriteLine(currentTask.task1("Simon Derppeberg"));
+        }
+    }
+}
---
+++ BasicExercises/Task.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BasicExercises
+{
+    class Task
+    {
+        public int task2(int a, int b)
+        {
+            return a + b;
+        }
+        public string task1(string Name)
+        {
+            return "Hello " + Name;
+        }
+    }
+}
