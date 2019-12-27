75dc402e3575a0f50ebb95ff864d9a47467c517c
Simon <simonhylleberg@gmail.com>
2019-12-27 19:50:59 +0100

    derpdederp

--- BasicExercises/Program.cs
+++ BasicExercises/Program.cs
@@ -7,10 +7,29 @@ namespace BasicExercises
         static void Main(string[] args)
         {
             Task currentTask = new Task();
+            //10
+            Console.WriteLine(currentTask.task10(5, 6, 7));
+            //9
+          //  Console.WriteLine(currentTask.task9(10, 15, 20, 30));
+            //8
+         //   currentTask.task8(5);
+            //7
+        //    Console.WriteLine(currentTask.task7(25, 4));
+            //6
+           // Console.WriteLine(currentTask.task6(2, 3, 6));
+            //5
+           // Console.WriteLine(currentTask.task5(2, 5));
+            //4
+          /*  Console.WriteLine(currentTask.task4(1));
+            Console.WriteLine(currentTask.task4(2));
+            Console.WriteLine(currentTask.task4(3));
+            Console.WriteLine(currentTask.task4(4));*/
+            //3
+            //   Console.WriteLine(currentTask.task3(27, 3));
             //2
-            Console.WriteLine(currentTask.task2(2, 7));
+            //   Console.WriteLine(currentTask.task2(2, 7));
             //1
-           // Console.WriteLine(currentTask.task1("Simon Derppeberg"));
+            // Console.WriteLine(currentTask.task1("Simon Derppeberg"));
         }
     }
 }
--- BasicExercises/Task.cs
+++ BasicExercises/Task.cs
@@ -6,6 +6,48 @@ namespace BasicExercises
 {
     class Task
     {
+        public string task10(int a, int b, int c)
+        {
+            return ((a + b) * c).ToString() + " " + ((a * b) + (b * c)).ToString();
+        }
+        public int task9(int a, int b, int c, int d)
+        {
+            return (a + b + c + d) / 4;
+        }
+        public void task8(int a)
+        {
+            for (int i = 0; i <= 10; i++)
+            {
+                Console.WriteLine(a * i);
+            }
+        }
+        public string task7(int a, int b)
+        {
+            return (a + b).ToString() + " " + (a - b).ToString() + " " + (a * b).ToString() + " " + (a / b).ToString() + " " + (a % b).ToString();
+        }
+        public int task6(int a, int b, int c)
+        {
+            return a * b * c;
+        }
+        public string task5(int a, int b)
+        {
+            return b.ToString() + " " + a.ToString();
+        }
+        public int task4(int a)
+        {
+            switch(a)
+            {
+                case 1: return -1 + 4 * 6;
+                case 2: return (35 + 5) % 7;
+                case 3: return 14 + -4 * 6 / 11;
+                case 4: return 2 + 15 / 6 * 1 - 7 % 2;
+                default: return 0;
+            }
+        }
+        public int task3(int a, int b)
+        {
+            return a / b;
+        }
         public int task2(int a, int b)
         {
             return a + b;
